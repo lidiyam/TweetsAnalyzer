2850a8b6b52615d2a1a6ce22823479963462fc03
Lidiya Murakhovs'ka <lidiya.murakhovskaya@gmail.com>
2017-02-17 00:39:25 -0500

    adding elasticsearch setup

--- build.sbt
+++ build.sbt
@@ -6,5 +6,8 @@ scalaVersion := "2.10.6"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.6" % "test"
 libraryDependencies += "org.apache.spark" % "spark-core_2.10" % "1.3.1"
+// https://mvnrepository.com/artifact/org.elasticsearch/elasticsearch-spark_2.10
+libraryDependencies += "org.elasticsearch" % "elasticsearch-spark_2.10" % "2.1.0.Beta3"
+
 
     
--- src/main/scala/TweetsAnalyzer.scala
+++ src/main/scala/TweetsAnalyzer.scala
@@ -1,15 +1,33 @@
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.elasticsearch.spark._
+import org.elasticsearch.spark.rdd.EsSpark
 
 object TweetsAnalyzer {
+  case class Trip(departure: String, arrival: String)
+
   def main(args: Array[String]) {
-    val logFile = "/Users/lidiyam/Developer/untitled/README.md"
+    val logFile = "/Users/lidiyam/Developer/tweets-analyzer/TweetsAnalyzer/README.md"
     val conf = new SparkConf().setAppName("Tweets Analyzer")
+    conf.set("es.nodes", "localhost")
+    conf.set("es.port", "9200")
+    conf.set("es.index.auto.create", "true")
     val sc = new SparkContext(conf)
+
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
     println(s"Lines with a: $numAs, Lines with b: $numBs")
+
+
+    val upcomingTrip = Trip("OTP", "SFO")
+    val lastWeekTrip = Trip("MUC", "OTP")
+
+    val rdd = sc.makeRDD(Seq(upcomingTrip, lastWeekTrip))
+    EsSpark.saveToEs(rdd, "spark/docs")
+
+    //sc.makeRDD(Seq(numbers, airports)).saveToEs("spark/docs")
+
     sc.stop()
   }
 
